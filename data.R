1fdf80178b9015e55810e72e32939cdc55eaccb4
Yanglu4 <50918284+Yanglu4@users.noreply.github.com>
2019-05-22 19:48:00 -0400

    Delete data.R

--- data.R
+++
@@ -1,7 +0,0 @@
-# Problem A12
-Y <- c(76,160,6,25,114,181,11,48)
-G <- factor(c("M","M","M","M","F","F","F","F"),levels=c("M","F"))
-I <- factor(c("Yes","Yes","No","No","Yes","Yes","No","No"),levels=c("Yes","No"))
-H <- factor(c("Yes","No","Yes","No","Yes","No","Yes","No"),levels=c("Yes","No"))
-GSS <- data.frame(cbind(Y,G,I,H))
-
